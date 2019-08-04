47c3c997f73a14ac75155d78363b3fe0fa9b1d8e
sughosneo <sughos@outlook.com>
2019-08-04 17:06:32 +0530

    #7

--- dynamodb/Constant.py
+++ dynamodb/Constant.py
@@ -1,3 +1,7 @@
+'''
+    In your actual production code you might not be hard coding this access key and secret access key value.
+    You would possibly be taking these values from your environment variable.
+'''
 class Constant:
 
     DYNAMO_DB_DEV_ACCESS_KEY_ID = ""
